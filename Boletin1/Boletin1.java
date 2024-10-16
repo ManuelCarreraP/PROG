665969f3aec1d0589af2111e9adc648a1807dda4
Manuel Carrera Pazó <mcarrerapazo@danielcastelao.org>
2024-10-16 13:59:14 +0200

    Boletin 1

---
+++ Boletin1/Boletin1.java
@@ -0,0 +1,40 @@
+package Boletin1;
+
+public class Boletin1 {
+    public static void main(String[] args) {
+
+        System.out.println("Ej1");
+        int a = ((3 + 2) % 2 - 15) / 2 * 5;
+        System.out.println("a) ((3 + 2) % 2 - 15) / 2 * 5 = " + a);
+
+        int b = (6 + (6) / 7) + 35 / 2 -8 * 5 / 4 * 2;
+        System.out.println("b) (6 + (6) / 7) + 35 / 2 -8 * 5 / 4 * 2 = " + b);
+
+        int c = 3 + 6 * 14 % 3;
+        System.out.println("c) 3 + 6 * 14 % 3 = " + c);
+
+        int d = 8 + 7 * 3 + 4 * 6 /2 % 4;
+        System.out.println("d) 8 + 7 * 3 + 4 * 6 /2 % 4 = " + d);
+
+        int e = 27 % 4 +15 / 4;
+        System.out.println("e) 27 % 4 +15 / 4 = " + e);
+
+        int f = 37 / 16 - 2;
+        System.out.println("f) 37 / 4² - 2 = " + f);
+
+        int g = 9 * 2 / 3 * 25 * 3;
+        System.out.println("g) 9 * 2 / 3 * 25 * 3 = " + g);
+
+        int h = (7 * 3 - 4 * 4) * 2 / 4 * 2;
+        System.out.println("h) 7 * 4 * 4 * 2 / 4 * 2 = " + h);
+        System.out.println(" ");
+
+        System.out.println("Ej3");
+
+        System.out.println(" ");
+
+        System.out.println("Ej5");
+
+
+    }
+}
